0c2ef1b514f8b70ee6bca52149bb27b96f1b4dfa
Farhan1994 <farhan.yen@northsouth.edu>
2019-03-14 18:19:27 +0600

    Empty class with only the function signatures

---
+++ Spring2019/CSE427/Assignment1/JavaGenerics/src/javagenerics/GenericStack.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagenerics;
+
+/**
+ *
+ * @author Yen
+ */
+public class GenericStack<Object>{
+    private int top;
+    public GenericStack(){
+        
+    }
+    public void push(Object x)
+    {
+        
+    }
+    public Object pop()
+    {
+        return something;
+        
+    }
+    
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    
+}
